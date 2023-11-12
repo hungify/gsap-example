8afa4a5addbfe039d3fd8c0e2fac54254f3af771
hungify <nmhungify@gmail.com>
2023-11-12 17:43:45 +0700

    feat: init source by starter template

---
+++ src/components/ScrollToExplore.tsx
@@ -0,0 +1,9 @@
+export default function ScrollToExplore() {
+  return (
+    <div className="text-white">
+      <div className="flex flex-col items-center justify-center h-screen">
+        <p className="text-xl mt-4">Scroll down to explore</p>
+      </div>
+    </div>
+  );
+}
